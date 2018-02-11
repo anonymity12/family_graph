fe8499c91fbeadf2f0cd6017377a16d8fd5994a1
MMesch <MMesch@users.noreply.github.com>
2018-02-11 13:33:45 +0100

    initial version

---
+++ mindmaps/test.mm
@@ -0,0 +1,104 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="freeplane in d3.js" FOLDED="false" ID="ID_1849740984" CREATED="1518344937295" MODIFIED="1518344953730" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="node1" POSITION="right" ID="ID_1970681855" CREATED="1518344954161" MODIFIED="1518344956208">
+<edge COLOR="#ff0000"/>
+<node TEXT="dsa" ID="ID_115831064" CREATED="1518344970373" MODIFIED="1518344971226"/>
+<node TEXT="as" ID="ID_1467812022" CREATED="1518344971436" MODIFIED="1518344972178"/>
+<node TEXT="asd" ID="ID_1593901694" CREATED="1518344972638" MODIFIED="1518344973393"/>
+<node TEXT="asd" ID="ID_875768420" CREATED="1518344973725" MODIFIED="1518344974186"/>
+</node>
+<node TEXT="node2" POSITION="right" ID="ID_190708251" CREATED="1518344956398" MODIFIED="1518344957652">
+<edge COLOR="#0000ff"/>
+<node TEXT="asd" ID="ID_683208606" CREATED="1518344975265" MODIFIED="1518344975907"/>
+<node TEXT="asd" ID="ID_288543957" CREATED="1518344976185" MODIFIED="1518344976566"/>
+<node TEXT="asd" ID="ID_377208580" CREATED="1518344976846" MODIFIED="1518344977232"/>
+</node>
+<node TEXT="node3" POSITION="right" ID="ID_1034647433" CREATED="1518344957857" MODIFIED="1518344958928">
+<edge COLOR="#00ff00"/>
+<node TEXT="asd" ID="ID_215200900" CREATED="1518344980845" MODIFIED="1518344981561"/>
+<node TEXT="asd" ID="ID_1848974540" CREATED="1518344981819" MODIFIED="1518344982831"/>
+<node TEXT="asd" ID="ID_1880635036" CREATED="1518344982998" MODIFIED="1518344983616">
+<node TEXT="aasd" ID="ID_1632076769" CREATED="1518344984482" MODIFIED="1518344985546"/>
+<node TEXT="asd" ID="ID_1252678847" CREATED="1518344985810" MODIFIED="1518344986325">
+<node TEXT="asd" ID="ID_1744258436" CREATED="1518344986612" MODIFIED="1518344987154"/>
+<node TEXT="asd" ID="ID_1604328323" CREATED="1518344987553" MODIFIED="1518344988220"/>
+</node>
+</node>
+</node>
+<node TEXT="node4" POSITION="right" ID="ID_1376743625" CREATED="1518344959136" MODIFIED="1518344960220">
+<edge COLOR="#ff00ff"/>
+<node TEXT="dsa" ID="ID_985765330" CREATED="1518344960732" MODIFIED="1518344961723"/>
+<node TEXT="asd" ID="ID_555607528" CREATED="1518344961938" MODIFIED="1518344962457"/>
+<node TEXT="asd" ID="ID_1765380011" CREATED="1518344962781" MODIFIED="1518344963265">
+<node TEXT="asd" ID="ID_569076729" CREATED="1518344964002" MODIFIED="1518344964780"/>
+<node TEXT="asd" ID="ID_1890743143" CREATED="1518344965060" MODIFIED="1518344965440"/>
+<node TEXT="asd" ID="ID_375222591" CREATED="1518344965792" MODIFIED="1518344966174"/>
+<node TEXT="asd" ID="ID_1953388443" CREATED="1518344966498" MODIFIED="1518344966854"/>
+</node>
+</node>
+</node>
+</map>
